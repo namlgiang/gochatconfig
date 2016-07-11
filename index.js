46e66c57b5b5755b1d5c30945407b8dcb2e6d75d
Giang Nam <namle@kinssolutions.net>
2016-07-11 11:26:15 +0700

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+var servers = [];
+servers.push({
+  url: "http://104.199.146.157:8080/",
+  region: "asia"
+});
+
+app.get('/', function(req, res) {
+  res.send("Go Chat Server");
+});
+
+app.get('/servers', function(req, res) {
+  res.send(servers);
+});
+
+app.listen(8080, function(req, res) {
+  console.log("Listening to 8080")
+});
